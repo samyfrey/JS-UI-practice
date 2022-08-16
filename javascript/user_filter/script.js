3a21b5b81998d99550a7b805076764d8d9bc7773
samyfrey <samyfrey@yahoo.com>
2022-08-16 19:40:31 -0400

    calculator training

--- javascript/user_filter/script.js
+++ javascript/user_filter/script.js
@@ -1,53 +1,66 @@
+import axios from 'axios'
+
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
 
 getData()
 
-filter.addEventListener('input', e => filterData(e.target.value))
+filter.addEventListener('input', (e) => filterData(e.target.value))``
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
-    const { results } = await res.json()
-    
-    result.innerHTML = ''
-    results.forEach(user => {
-        const li = document.createElement('li')
-        listItems.push(li)
-        li.innerHTML= `
+	const res = await fetch('https://randomuser.me/api?results=50')
+	const { results } = await res.json()
+
+	result.innerHTML = ''
+	results.forEach((user) => {
+		const li = document.createElement('li')
+		listItems.push(li)
+		li.innerHTML = `
             <img src="${user.picture.large}" alt="${user.name.first}">
             <div class="user-info">
                 <h4>${user.name.first} ${user.name.last}</h4>
                 <p>${user.location.city}, ${user.location.country}</p>
             </div>
         `
-        result.appendChild(li)
-    })
+		result.appendChild(li)
+	})
 }
 
 function filterData(searchTerm) {
-    listItems.forEach(item =>{
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
-            item.classList.remove('hide')
-        } else {
-            item.classList.add('hide')
-        }
-    })
+	listItems.forEach((item) => {
+		if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+			item.classList.remove('hide')
+		} else {
+			item.classList.add('hide')
+		}
+	})
 }
 
 let slideIndex = 1
 showSlides(slideIndex)
 
-function plusSlides(n) {
-	showSlides((slideIndex += n))
+function plusSlides(selectIndex) {
+	showSlides((slideIndex += selectIndex))
 }
 
-function showSlides(n){
-    let slides = document.getElementsByClassName("slide")
-    if(n>slides.length){slideIndex = 1}
-    if(n<1){slideIndex = slides.length}
-    for (let i = 0; i<slides.length; i++){
-        slides[i].style.display = 'none'
-    }
-    slides[slideIndex-1].style.display='block'
-    console.log('slide index is', slideIndex);
+function showSlides(selectIndex) {
+	let slides = document.getElementsByClassName('slide')
+	if (selectIndex > slides.length) {
+		slideIndex = 1
+	}
+	if (selectIndex < 1) {
+		slideIndex = slides.length
+	}
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none'
+	}
+	slides[slideIndex - 1].style.display = 'block'
+	console.log('slide index is', slideIndex)
 }
+
+export const indexClients = () => {
+	return axios.get('http://localhost:4741/clients')
+}
+indexClients()
+
+console.log('index client is', indexClients)
